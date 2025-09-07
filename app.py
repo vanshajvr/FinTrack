ee090af276045c5e2a38b9a9e1a1218ff3f5f5c8
Vanshaj Verma <140025134+vanshajvr@users.noreply.github.com>
2025-09-07 12:54:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -79,4 +79,7 @@ def get_transactions():
     return jsonify(transactions_list), 200
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
